05b4dbf2b268999f20865d0bcbb634086bd0e742
231141 <231141@virtualwindow.co.za>
2024-07-14 19:33:45 +0200

    commit

--- Display Terminal/script.js
+++ Display Terminal/script.js
@@ -1,32 +1,24 @@
 let welcomeMessage = "Come in";
 const logParagraph = document.getElementById('logParagraph');
-
 var buttonCounter = 0;
 
 FakeLogger("I'm in");
 
-function RenameWelcomeHeader()
-{
-    var header = document.getElementById('welcomeheader');
-    header.innerHTML = welcomeMessage;
+function RenameWelcomeHeader() {
+  var header = document.getElementById('welcomeHeader');
+  header.innerHTML = welcomeMessage;
 }
 
-function FakeLogger(logMessage)
-{
-    console.log(logMessage);
-    logParagraph.innerHTML = logMessage;
+function FakeLogger(logMessage) {
+  console.log(logMessage);
+  logParagraph.innerHTML = logMessage;
 }
 
-function execute()
-{
-    buttonCounter += 1;
-    
-    if (buttonCounter == 1)
-    {
-        FakeLogger("You pressed me once!");
-    }
-    else
-    {
-        FakeLogger(`You have pressed me ${buttonCounter} times`);
-    }
+function execute() {
+  buttonCounter += 1;
+  if (buttonCounter == 1) {
+    FakeLogger("You pressed me once!");
+  } else {
+    FakeLogger(`You have pressed me ${buttonCounter} times`);
+  }
 }
